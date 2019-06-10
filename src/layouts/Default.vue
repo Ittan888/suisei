ad3d958dca027cda01685ff9d3c93ec25f9c93d6
板谷洋明 <hiroaki.itadani@mail.growth-d.co.jp>
2019-06-11 08:10:05 +0900

    init

---
+++ src/layouts/Default.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app dark class="default">
+    
+    <v-toolbar color="accent" dark fixed app>
+      <!-- <v-toolbar-title>キャスト予約</v-toolbar-title> -->
+      <v-layout align-center>
+        <h1 class="logo ml-5"><img src="/siteid.png" alt=""></h1>
+      </v-layout>
+      <v-spacer></v-spacer>
+      <show-at :breakpoints="breakpoints" breakpoint="small">
+        <v-toolbar-side-icon @click.stop="drawer.isOpen = !drawer.isOpen"></v-toolbar-side-icon>
+      </show-at>
+    </v-toolbar>
+    
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout>
+          <v-flex>
+            <slot />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  data() {
+    return {
+      breakpoints: {
+        small: 768,
+        medium: Infinity,
+        large: Infinity
+      },
+      clipped: false,
+      fixed: false,
+      drawer: {
+        isOpen: true,
+        items: [
+          {
+            icon: "create",
+            title: "ダッシュボード",
+            exact: true,
+            to: "/"
+          },
+          {
+            icon: "create",
+            title: "seed",
+            exact: true,
+            to: "/seeds"
+          }
+        ]
+      },
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    }
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style lang="stylus">
+.default
+  background #212121 !important
+  .logo
+    width 75px
+    height 52px
+    img
+      width 100%
+</style>
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue'
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+import {showAt, hideAt} from 'vue-breakpoints'
+
+import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '~/assets/app.styl'
+import '~/assets/variables.styl'
+
+export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+  })
+
+  Vue.use(Vuetify, {
+    iconfont: 'fa',
+    theme: {
+      primary: colors.grey.darken4,
+      accent: colors.shades.black,
+      secondary: colors.amber.darken3,
+      info: colors.teal.lighten1,
+      warning: colors.amber.base,
+      error: colors.deepOrange.accent4,
+      success: colors.green.accent3
+    }
+  })
+
+  Vue.component('Layout', DefaultLayout)
+  Vue.component('show-at', showAt)
+  Vue.component('hide-at', hideAt)
+
+}
